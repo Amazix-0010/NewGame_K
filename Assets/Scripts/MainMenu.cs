01da9288cb3cd8f43d07dfdb308f03477d80c2a4
Amazix-0010 <65762218+Amazix-0010@users.noreply.github.com>
2020-06-10 13:09:32 +0200

    MainMenu
    Basic

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public string firstLevel;
+    // Start is called before the first frame update
+
+  /*  public void Continue()
+    {
+
+    }*/
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(firstLevel);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+        Debug.Log("Vypínám hru");
+    }
+
+}
