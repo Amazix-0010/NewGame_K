6353d5b0a34403bc614022b772e136084d3bf69d
Amazix-0010 <65762218+Amazix-0010@users.noreply.github.com>
2020-06-15 12:13:31 +0200

    Level,Menu,Enemy_IMP

--- Assets/Scripts/LoseMenu.cs
+++ Assets/Scripts/LoseMenu.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class LoseMenu : MonoBehaviour
 {
     public static LoseMenu instance;
+
+    public string _level_1;
 
     private void Awake()
     {
@@ -21,5 +24,21 @@ public class LoseMenu : MonoBehaviour
     void Update()
     {
         Cursor.lockState = CursorLockMode.None;
+    }
+
+    public void ResetRound()
+    {
+
+    }
+
+    public void ResetGame()
+    {
+        SceneManager.LoadScene(_level_1);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+        Debug.Log("Vypínám hru");
     }
 }
